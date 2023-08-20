e4e1fbcea55aa932de884f3a2ff848bfce4cac74
deveshidwivedi <120312681+deveshidwivedi@users.noreply.github.com>
2023-08-20 14:46:21 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ let weather={
         .then((response)=>response.json())
         .then((data)=>this.displayWeather(data));
     },
+  displayWeather: function(data){
+        const {name}= data;
+        const{icon, description}= data.weather[0];
+        const{temp, humidity}= data.main;
+        const{speed}= data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector("#city").innerHTML
+    }
+};
